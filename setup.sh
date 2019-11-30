0b05a7c860a8ddd68a060c3f6b9c676c6b209d7f
Ryan Lee <ry4n.l3e@gmail.com>
2019-11-30 22:06:22 +0800

    mozilla

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade
+
 echo " "
-echo "---------- Setting up wifi ----------"
+echo "---------- Setting up hardware ----------"
 echo " "
 
 sudo apt install linux-headers-generic network-manager wireless-tools x11-xserver-utils blueman xbacklight compiz dconf-cli uuid-runtime
@@ -10,34 +12,11 @@ echo " "
 echo "---------- Transferring configs -----------"
 echo " "
 
-if [ ! -d $HOME/.bak ]; then
-    echo "Creating $HOME/.bak"
-    mkdir $HOME/.bak
-fi
-
-if [ -f $HOME/.bashrc ]; then
-    mv $HOME/.bashrc $HOME/.bak/.bashrc.bak
-fi
-
-if [ -f $HOME/.tmux.conf ]; then
-    mv $HOME/.tmux.conf $HOME/.bak/.tmux.conf.bak
-fi
-
-if [ -f $HOME/.tmux-reset ]; then
-    mv $HOME/.tmux-reset $HOME/.bak/.tmux-reset.bak
-fi
-
-if [ -f $HOME/.tmux-status ]; then
-	mv $HOME/.tmux-status $HOME/.bak/.tmux-status.bak
-fi
-
-if [ -d $HOME/.vimrc ]; then
-	mv $HOME/.vimrc $HOME/.bak/.vimrc.bak
-fi
-
-if [ -d $HOME/.Xresources ]; then
-	mv $HOME/.Xresources $HOME/.bak/.Xresources.bak
-fi
+ln -fs $HOME/.dotfiles/.bashrc $HOME/.bashrc
+ln -fs $HOME/.dotfiles/.tmux-keybinds $HOME/.tmux-keybinds
+ln -fs $HOME/.dotfiles/.tmux-reset $HOME/.tmux-reset
+ln -fs $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/.dotfiles/.vimrc $HOME/.vimrc
 
 echo " "
 echo "---------- Installing i3-Gaps ----------"
@@ -45,10 +24,7 @@ echo " "
 sudo add-apt-repository ppa:jonathonf/i3
 sudo apt update
 sudo apt install i3-gaps
-
-cp -r $HOME/config/bumblebee-status $HOME/.config/
-cp -r $HOME/config/i3/ $HOME/.config/
-rsync -vz --exclude '.git' $HOME/config/\.* $HOME
+ln -fs $HOME/.dotfiles/i3/config $HOME/.config/i3/config
 
 echo " "
 echo "---------- Installing Vim 8.1 ----------"
@@ -78,10 +54,21 @@ curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
 echo " "
+echo "---------- Installing Tilix ----------"
+echo " "
+
+sudo add-apt-repository ppa:webupd8team/terminix
+sudu apt update
+dconf load /com/gexperts/Tilix/ < $HOME/.dotfiles/terminix.dconf
+dconf reset -f /com/gexperts/Terminix/
+ln -fs $HOME/.dotfiles/tilix $HOME/.config/tilix
+
+echo " "
 echo "---------- Installing other good to have packages -----------"
 
-sudo apt install fonts-font-awesome firefox ncdu tree htop feh rxvt-unicode-256color
+sudo apt install chromium fonts-font-awesome firefox ncdu tree htop feh rxvt-unicode-256color tilix
 
+ln -fs $HOME/.dotfiles/.mozilla	$HOME/.mozilla
 mkdir $HOME/Pictures
 cp $HOME/config/dracula.png $HOME/Pictures/
 
@@ -91,3 +78,8 @@ echo " "
 
 sudo apt install python-pip
 pip install --user psutil netifaces
+
+echo " "
+echo "---------- Installing Python 3 ----------"
+sudo apt install python3-pip python3 python3-dev
+pip install --user virtualenv
